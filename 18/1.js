45a564864b92ab626725dd32f7507636126a6266
Shellme3 <158443574+Shellme3@users.noreply.github.com>
2024-05-30 10:42:27 +0300

    1

---
+++ 18/1.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 10;
+
+let c = a + b;
+
+let d = a - b;
+
+let e = a * b;
+
+let f = a / b;
+
+alert(c);
+alert(d);
+alert(e);
+alert(f);
---
+++ 18/2.js
@@ -0,0 +1,5 @@
+let c = 10
+let d = 5
+
+let result = c + d
+alert(result);
