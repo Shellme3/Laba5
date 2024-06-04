5e5969f106cd8b8e7f5ff422722b97788fa4f8da
Shellme3 <158443574+Shellme3@users.noreply.github.com>
2024-06-04 10:01:14 +0300

    1

---
+++ 44/1.js
@@ -0,0 +1,5 @@
+alert( Number('2') + Number('3') ); // 5
+
+alert( 2 + Number('3') ); // 5
+
+alert( '2' + Number('3') ); // 23
---
+++ 46/1.js
@@ -0,0 +1,3 @@
+let a = parseInt("5px")
+let b = parseInt("6px")
+alert(a+b+"px");
